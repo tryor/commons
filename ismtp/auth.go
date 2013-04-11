dffca5f08f3fc62c46d06e87376c3dd5147dfb96
trygo <trywen@qq.com>
2013-04-11 09:04:24 +0800

    smtp
    1. 重新实现PlainAuth，去掉server.TLS检查
    2. 增加LoginAuth, 解决某些邮件器只支持AUTH LOG方式问题
    3. 重写net/smtp/smtp.go，解决里面对某些smtp服务器支持不好的bug,比如smtp-ent.21cn.com

---
+++ ismtp/auth.go
@@ -0,0 +1,63 @@
+package ismtp
+
+import (
+	"bytes"
+	"errors"
+	"net/smtp"
+)
+
+type plainAuth struct {
+	identity, username, password string
+	host                         string
+}
+
+func PlainAuth(identity, username, password, host string) smtp.Auth {
+	return &plainAuth{identity, username, password, host}
+}
+
+func (a *plainAuth) Start(server *smtp.ServerInfo) (string, []byte, error) {
+	//if !server.TLS {
+	//	return "", nil, errors.New("unencrypted connection")
+	//}
+	if server.Name != a.host {
+		return "", nil, errors.New("wrong host name")
+	}
+	resp := []byte(a.identity + "\x00" + a.username + "\x00" + a.password)
+	return "PLAIN", resp, nil
+}
+
+func (a *plainAuth) Next(fromServer []byte, more bool) ([]byte, error) {
+	if more {
+		// We've already sent everything.
+		return nil, errors.New("unexpected server challenge")
+	}
+	return nil, nil
+}
+
+type loginAuth struct {
+	username, password string
+	host               string
+}
+
+func LoginAuth(username, password, host string) smtp.Auth {
+	return &loginAuth{username, password, host}
+}
+
+func (a loginAuth) Start(server *smtp.ServerInfo) (string, []byte, error) {
+	if server.Name != a.host {
+		return "", nil, errors.New("wrong host name")
+	}
+	return "LOGIN", nil, nil
+}
+
+func (a loginAuth) Next(fromServer []byte, more bool) ([]byte, error) {
+	if more {
+		idname := bytes.ToLower(fromServer)
+		if bytes.Equal([]byte("username:"), idname) {
+			return []byte(a.username), nil
+		} else if bytes.Equal([]byte("password:"), idname) {
+			return []byte(a.password), nil
+		}
+	}
+	return nil, nil
+}
